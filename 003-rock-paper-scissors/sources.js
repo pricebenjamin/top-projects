9687eac2936033765ce466641efbfb405488efd2
Ben Price <price.benjamindaniel@gmail.com>
2024-02-15 15:03:36 -0800

    Outline solution using comment-code

---
+++ 003-rock-paper-scissors/sources.js
@@ -0,0 +1,16 @@
+main();
+
+function main() {
+    // Define valid choices
+    // Greet user, explain input criteria
+
+    // Begin game loop
+        // Generate computer's choice
+        // Begin user input loop
+            // Get input from user
+            // Sanitize input
+            // Validate input
+        // End user input loop
+        // Determine outcome for this round
+    // End game loop
+}
