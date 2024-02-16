bd030b76a41b4a5cd5a491d434942d46f0cd5b68
Ben Price <price.benjamindaniel@gmail.com>
2024-02-15 16:11:21 -0800

    Add logic for computer's choice and user input

--- 003-rock-paper-scissors/sources.js
+++ 003-rock-paper-scissors/sources.js
@@ -5,13 +5,7 @@ function main() {
     greet_user();
 
     // Begin game loop
-        // Generate computer's choice
-        // Begin user input loop
-            // Get input from user
-            // Sanitize input
-            // Validate input
-        // End user input loop
-        // Determine outcome for this round
+        play_round();
     // End game loop
 
     function greet_user() {
@@ -24,5 +18,33 @@ The valid choices are: ${choices.join(", ")}
 Note: spelling matters but capitalization will be ignored.`
         );
         console.log(msg);
+    }
+
+    function play_round() {
+        const computer_choice = computer_pick_random_choice();
+        const user_choice = get_user_choice();
+        // TODO: Determine outcome for this round
+        console.log(`Computer: ${computer_choice}`);
+        console.log(`User: ${user_choice}`);
+    }
+
+    function computer_pick_random_choice() {
+        const random_idx = Math.floor(Math.random() * choices.length);
+        return choices[random_idx];
+    }
+
+    function get_user_choice() {
+        while (true) {
+            const raw_input = prompt(`Your choice:`)
+            const sanitized = raw_input.toLowerCase();
+
+            // validate user input
+            if (choices.includes(sanitized)) {
+                return sanitized;
+            } else {
+                console.log(`Info: invalid choice: ${raw_input}`);
+                console.log(`valid choices: ${choices.join(", ")}`);
+            }
+        }
     }
 }
