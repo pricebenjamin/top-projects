f325f530f31ddf6bc9c4628a27ccccb6643e25f5
Ben Price <price.benjamindaniel@gmail.com>
2024-02-20 19:52:26 -0800

    Implement sketching

--- 004-etch-a-sketch/sources.js
+++ 004-etch-a-sketch/sources.js
@@ -4,10 +4,6 @@ const grid_size = document.querySelector('#grid-size');
 grid_size.addEventListener('change', main);
 
 const grid_root = document.querySelector('.grid');
-grid_root.addEventListener('mouseover', (e) => {
-    if (!e.target.id) return;
-    console.log(e.target.id);
-});
 
 function main() {
     grid_root.replaceChildren([]);
@@ -24,13 +20,41 @@ function createGridRow(size, row_idx) {
     for (let i = 0; i < size; i++) {
         const cell = document.createElement('div');
         cell.classList.toggle('grid-cell');
-        cell.style.backgroundColor = `hsl( ${randomInt(360)} 100% 97% )`;
+        cell.style.backgroundColor = '#FFFFFF';
         cell.id = `id${size * row_idx + i}`;
         row.appendChild(cell);
     }
     return row;
 }
 
-function randomInt(max) {
-    return Math.floor(Math.random() * max);
+grid_root.addEventListener('mouseover', decreaseBrightness);
+
+function decreaseBrightness(event) {
+    const init_color = (window
+        .getComputedStyle(event.target)
+        .getPropertyValue('background-color')
+    );
+    event.target.style.backgroundColor = dim(init_color);
+}
+
+function dim(color_str) {
+    const min_value = 40;
+    const step_size = 0.2;
+    let [r, g, b] = parseColor(color_str);
+    r = Math.max(min_value, Math.floor(r * (1 - step_size)));
+    g = Math.max(min_value, Math.floor(g * (1 - step_size)));
+    b = Math.max(min_value, Math.floor(b * (1 - step_size)));
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function parseColor(rgb_str) {
+    const segments = rgb_str.split(",")
+    const numbers = segments.map(keepDigits)
+    return numbers.map(Number);
+}
+
+function keepDigits(str) {
+    return str.split("").filter(c =>
+        '0' <= c && c <= '9'
+    ).join("");
 }
