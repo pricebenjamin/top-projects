49594c914253d68d95ff8b70094a20fecd66f295
Ben Price <price.benjamindaniel@gmail.com>
2024-04-09 12:02:23 -0700

    Add confirmation dialog on project deletion

--- 011-todo-list/src/App/Components/Header/Header.tsx
+++ 011-todo-list/src/App/Components/Header/Header.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Project } from "@components/Project";
+import { ConfirmationModal } from "@components/Modal";
 import "./Header.css";
 
 interface HeaderProps {
@@ -14,21 +16,37 @@ export function Header({
   onProjectTitleChange,
   onTodoCreate,
 }: HeaderProps) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
   return (
-    <header className="header">
-      <h1 className="active-project">
-        <input
-          type="text"
-          value={project.title}
-          onChange={(event) =>
-            onProjectTitleChange(project.id, event.target.value)
-          }
-        />
-      </h1>
-      <nav>
-        <button onClick={() => onTodoCreate(project.id)}>Add Todo</button>
-        <button onClick={() => onProjectDelete(project.id)}>Delete</button>
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="active-project">
+          <input
+            type="text"
+            value={project.title}
+            onChange={(event) =>
+              onProjectTitleChange(project.id, event.target.value)
+            }
+          />
+        </h1>
+        <nav>
+          <button onClick={() => onTodoCreate(project.id)}>Add Todo</button>
+          <button onClick={() => setShowDeleteConfirmation(true)}>
+            Delete
+          </button>
+        </nav>
+      </header>
+      <ConfirmationModal
+        show={showDeleteConfirmation}
+        heading="Are you sure?"
+        message={`Delete: ${project.title}`}
+        onAccept={() => {
+          onProjectDelete(project.id);
+          setShowDeleteConfirmation(false);
+        }}
+        onReject={() => setShowDeleteConfirmation(false)}
+      />
+    </>
   );
 }
