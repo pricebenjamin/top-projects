89af2c111d0147a1df80cc94c043ae6b0bdc744d
Ben Price <price.benjamindaniel@gmail.com>
2024-04-30 15:18:32 -0700

    Update FleetStatus design

--- 014-battleship/src/App/Components/FleetStatus/FleetStatus.tsx
+++ 014-battleship/src/App/Components/FleetStatus/FleetStatus.tsx
@@ -6,20 +6,37 @@ interface FleetStatusProps {
 }
 
 export function FleetStatus({ player }: FleetStatusProps) {
+  const activeShips = [];
+  const sunkShips = [];
+
+  for (const ship of player.deployedShips) {
+    if (ship.coordinates.some((coord) => player.squares[coord] !== "hit")) {
+      activeShips.push(ship);
+      continue;
+    }
+    sunkShips.push(ship);
+  }
+
   return (
     <div className="fleet-status">
-      <ul>
-        {player.deployedShips.map((ship) => {
-          const alive = ship.coordinates.some(
-            (coord) => player.squares[coord] !== "hit"
-          );
-          return (
-            <li key={ship.class} style={{ color: alive ? "green" : "red" }}>
-              {ship.class}
-            </li>
-          );
-        })}
-      </ul>
+      <h2 className="score-heading">Score</h2>
+      <div className="score">{sunkShips.length}</div>
+      {activeShips.length > 0 && (
+        <div className="active">
+          <h2>Active</h2>
+          {activeShips.map((ship) => (
+            <div>{ship.class}</div>
+          ))}
+        </div>
+      )}
+      {sunkShips.length > 0 && (
+        <div className="sunk">
+          <h2>Sunk</h2>
+          {sunkShips.map((ship) => (
+            <div>{ship.class}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
